a7be80e949e63eaf2e5fcc1e8ffa7ee7ea1a2757
Batou99 <lorenzo.lopez@intec.es>
2016-03-19 12:00:47 +0000

    Extract monoidness to a separate type

--- monoids_in_practice.hs
+++ monoids_in_practice.hs
@@ -1,57 +1,49 @@
 import Text.Printf
 
-data OrderLine
-  = ProductLine { productCode :: String, productQuantity :: Int, price :: Float, lineTotal :: Float }
-  | TotalLine { totalQuantity :: Int, orderTotal :: Float }
-  | EmptyLine
-
+data OrderLine = OrderLine { productCode :: String, productQuantity :: Int, price :: Float, lineTotal :: Float }
+data TotalLine = TotalLine { totalQuantity :: Int, orderTotal :: Float }
 
 instance Show OrderLine where
-  show (ProductLine plCode plQuantity plPrice plTotal) =
+  show (OrderLine plCode plQuantity plPrice plTotal) =
     printf "%-10s %5i @%4g each %6g" plCode plQuantity plPrice plTotal
+
+instance Show TotalLine where
   show (TotalLine tlQuantity tlTotal) =
     printf "%-10s %5i            %6g" "TOTAL" tlQuantity tlTotal
 
 
 sampleLines :: [OrderLine]
 sampleLines =
-  [ ProductLine { productCode = "AAA", productQuantity = 2, price = 2.99, lineTotal = 5.98 },
-    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
-    ProductLine { productCode = "CCC", productQuantity = 3, price = 3.99, lineTotal = 11.97 }
+  [ OrderLine { productCode = "AAA", productQuantity = 2, price = 2.99, lineTotal = 5.98 },
+    OrderLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    OrderLine { productCode = "CCC", productQuantity = 3, price = 3.99, lineTotal = 11.97 }
   ]
 
 
 moreSampleLines :: [OrderLine]
 moreSampleLines =
-  [ ProductLine { productCode = "DDD", productQuantity = 4, price = 4.99, lineTotal = 19.96 },
-    ProductLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
-    ProductLine { productCode = "EEE", productQuantity = 2, price = 2.00, lineTotal = 4.00 }
+  [ OrderLine { productCode = "DDD", productQuantity = 4, price = 4.99, lineTotal = 19.96 },
+    OrderLine { productCode = "BBB", productQuantity = 1, price = 1.99, lineTotal = 1.99 },
+    OrderLine { productCode = "EEE", productQuantity = 2, price = 2.00, lineTotal = 4.00 }
   ]
 
 
-addLine :: OrderLine -> OrderLine -> OrderLine
-addLine EmptyLine line = line
-addLine line EmptyLine  = line
-addLine (ProductLine _ plQuantity _ plTotal) (TotalLine tlQuantity tlTotal) =
+add :: TotalLine -> TotalLine -> TotalLine
+add line1 line2 =
   TotalLine {
-    totalQuantity = plQuantity + tlQuantity,
-    orderTotal = plTotal + tlTotal
+    totalQuantity = totalQuantity line1 + totalQuantity line2,
+    orderTotal = orderTotal line1 + orderTotal line2
   }
-addLine (TotalLine tlQuantity tlTotal) (ProductLine _ plQuantity _ plTotal) =
-  TotalLine {
-    totalQuantity = plQuantity + tlQuantity,
-    orderTotal = plTotal + tlTotal
-  }
-addLine (TotalLine tlQuantity1 tlTotal1) (TotalLine tlQuantity2 tlTotal2) =
-  TotalLine {
-    totalQuantity = tlQuantity1 + tlQuantity2,
-    orderTotal = tlTotal1 + tlTotal2
-  }
-addLine line1 line2 =
-  TotalLine {
-    totalQuantity = productQuantity line1 + productQuantity line2,
-    orderTotal = lineTotal line1 + lineTotal line2
-  }
+
+
+zero :: TotalLine
+zero =
+  TotalLine 0 0
+
+
+toTotalLine :: OrderLine -> TotalLine
+toTotalLine line =
+  TotalLine { totalQuantity = productQuantity line, orderTotal = lineTotal line }
 
 
 class Monoid m where
@@ -61,9 +53,9 @@ class Monoid m where
   mconcat = foldr mappend mempty
 
 
-instance Monoid OrderLine where
-  mempty = EmptyLine
-  mappend = addLine
+instance Monoid TotalLine where
+  mempty = zero
+  mappend = add
 
 
 main = do
@@ -75,8 +67,8 @@ main = do
   putStrLn "----------------------------------"
   print bigTotal
   where
-    subtotal = mconcat sampleLines
-    bigTotal = mconcat $ moreSampleLines ++ [subtotal]
+    subtotal = mconcat $ map toTotalLine sampleLines
+    bigTotal = mconcat $ map toTotalLine moreSampleLines ++ [subtotal]
 
 
 
