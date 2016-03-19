2c527c771ae924d6c149d8aaf9e4de8d3d99aec6
Batou99 <lorenzo.lopez@intec.es>
2016-03-19 17:36:06 +0000

    Basic count example

---
+++ monoid_homomorphisms.hs
@@ -0,0 +1,19 @@
+import Text.Printf
+
+
+add :: String -> String -> String
+add = (++)
+
+
+wordCount :: String -> Int
+wordCount t =
+  length $ words t
+
+
+page :: String -> Int -> String
+page word numberOfTimes =
+  unwords $ replicate numberOfTimes word
+
+
+main = 
+  printf "The word count is %6i" $ wordCount $ page "Hello" 1000
