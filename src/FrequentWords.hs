afe55f088249b25310e3fb3580e41e130577cf05
Batou99 <lorenzo.lopez@intec.es>
2016-03-26 19:14:21 +0000

    Second iteration mostFrequentWord

--- src/FrequentWords.hs
+++ src/FrequentWords.hs
@@ -1,10 +1,20 @@
 module FrequentWords (main) where
 
 import qualified Data.Map as Map
+import qualified Data.List as List
 
 
-mostFrequentWord :: String -> Map.Map String Int
+mostFrequentWord :: String -> String
 mostFrequentWord page =
+  fst $ head sortedList
+  where
+    wordsmap = wordFrequencies page
+    wordslist = Map.toList wordsmap
+    sortedList = List.sortBy (\(_,v1) (_,v2) -> compare v1 v2) wordslist 
+
+
+wordFrequencies :: String -> Map.Map String Int
+wordFrequencies page =
   foldl foldF Map.empty list
   where
     list = words page
