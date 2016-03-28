52bd1367c7be0ca06b062db65465e43270a4cf76
Batou99 <lorenzo.lopez@intec.es>
2016-03-28 10:49:16 +0000

    Basic Monoidal Char

---
+++ src/MonoidalChar.hs
@@ -0,0 +1,20 @@
+module MonoidalChar where
+
+import Data.Char
+
+type MChar = String
+
+
+toMChar :: Char -> String
+toMChar x = [x]
+
+
+add :: MChar -> MChar -> MChar
+add = (++)
+
+
+main :: IO ()
+main = do
+  let a = toMChar 'a'
+      b = toMChar 'b'
+  putStrLn $ add a b
