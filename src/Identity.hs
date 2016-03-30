9429a325ae1a8dfa59ad30a84d8e1319341e5519
Batou99 <lorenzo.lopez@intec.es>
2016-03-30 07:59:07 +0000

    Abstract the identity pattern

--- src/Identity.hs
+++ src/Identity.hs
@@ -1,5 +1,6 @@
 module Identity where
 
+import Data.Monoid
 
 data PositiveNumberOrIdentity
   = Positive Int
@@ -11,6 +12,19 @@ instance Monoid PositiveNumberOrIdentity where
   mappend Zero i = i
   mappend i Zero = i
   mappend (Positive i1) (Positive i2) = Positive (i1 + i2)
+
+-- ABSTRACT THE PATTERN
+
+data Monoid a => NormalOrIdentity a
+  = Normal a
+  | Empty
+
+
+instance Monoid a => Monoid (NormalOrIdentity a) where
+  mempty = Empty
+  mappend Empty x = x
+  mappend x Empty = x
+  mappend (Normal x) (Normal y) = Normal (x <> y)
 
 
 main :: IO()
